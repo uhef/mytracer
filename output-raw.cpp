e39141407ea5808b8fc9edbf85474bd0945b2247
uhef <tuomas.jarvensivu@iki.fi>
2015-02-15 17:39:46 +0200

    Make the other sphere yellow

--- output-raw.cpp
+++ output-raw.cpp
@@ -110,7 +110,7 @@ float calculateLambert(Vector sphereCenter, Vector intersection) {
 
 void renderImage(uint8_t* pixels) {
   spheres.push_back(std::make_pair(Vector(0.0f, 0.3f, -1.0f), Color(1.0f, 0.0f, 0.0f)));
-  spheres.push_back(std::make_pair(Vector(0.0f, -0.3f, -1.0f), Color(1.0f, 0.0f, 0.0f)));
+  spheres.push_back(std::make_pair(Vector(0.0f, -0.3f, -1.0f), Color(0.96f, 0.94f, 0.32f)));
   uint8_t* p = pixels;
   for(int i = 0; i < 32; ++i) {
     for(int j = 0; j < 32; ++j) {
